cfb2d03b90fd88c2c0e728beb0c0743154a697d5
Ketan Alluri <ketan.mitmpl2026@learner.manipal.edu>
2026-09-01 19:30:22 +0530

    Update q1.py

--- q1.py
+++ q1.py
@@ -9,4 +9,4 @@ for string in string_list:
     for char in string.lower():
         if char.isalpha():
             char_counts[char] = char_counts.get(char, 0) + 1
-print("Alphabet counts:", char_counts)
+print("Alphabet counts:",sorted(char_counts.items()))
